f0c0f91a67eb3513e500e868e02756463e5b5a55
Teoretik <kobzarev.dani@yandex.ru>
2023-11-23 23:17:18 +0400

    display fits file

--- IOSolarData/imagesStorage.py
+++ IOSolarData/imagesStorage.py
@@ -1,15 +1,30 @@
 import os
 
-from PyQt5.QtGui import QImage
-from astropy.io import fits
+import sunpy.visualization.colormaps.cm
 
+import numpy as np
+from PyQt5.QtGui import QImage, QColor
+from astropy.io import fits
 
 class ImagesStorage:
     def __init__(self):
         self.path_of_files_in_storage = list(filter((lambda f : "image" in f), os.listdir("Images")))
 
+    def read_test_image(self, w, h):
+        print(sunpy.visualization.colormaps.cm.sdoaia171(1))
+        data = np.ones((w, h, 3), dtype=np.uint8)
+        for x in range(w):
+            for y in range(h):
+                data[x][y][0] = 254
+                data[x][y][1] = 150
+                data[x][y][2] = 1
+        return QImage(data, data.shape[1], data.shape[0], 3*w, QImage.Format_RGB888)
 
     def read_image_by_index(self, index:int):
         path = self.path_of_files_in_storage[index]
-        data = fits.open("Images/" + path)[1].data
-        return QImage(data, data.shape[1], data.shape[0], QImage.Format_Indexed8)
+        readed_data = fits.open("Images/" + path)[1].data
+        img_w = readed_data.shape[0]
+        img_h = readed_data.shape[1]
+        cm = sunpy.visualization.colormaps.cm.sdoaia193
+        a = np.array(255 * cm(readed_data), dtype=np.uint8)
+        return QImage(a, img_h, img_w, 4 * img_w, QImage.Format_RGBA8888)
