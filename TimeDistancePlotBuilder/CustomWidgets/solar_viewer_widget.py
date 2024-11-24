6587011c00db144c13e62133255aa30e9890ca1b
Teoretik <kobzarev.dani@yandex.ru>
2024-11-24 10:22:15 -0500

    refactoring zone_interesting, step 2

--- TimeDistancePlotBuilder/CustomWidgets/solar_viewer_widget.py
+++ TimeDistancePlotBuilder/CustomWidgets/solar_viewer_widget.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QLabel, QWidget
+from PyQt5.QtWidgets import QLabel, QWidget, QGraphicsScene, QGraphicsView, QGraphicsProxyWidget
 from PyQt5.QtGui import QPalette, QColor, QPixmap, QImage, QPainter, QPen
 from PyQt5.QtCore import Qt, pyqtSignal, QPoint
 from TimeDistancePlotBuilder.Models.app_models import AppModel, ZoneInteresting
@@ -8,6 +8,83 @@ from TimeDistancePlotBuilder.CustomWidgets.zone_interesting_control_point_widget
 class SolarViewerWidget(QWidget):
     wheelScrollSignal = pyqtSignal(int, int)
     mouseMoveSignal = pyqtSignal(int, int)
+    
+    def __init__(self, solar_viewer_controller, app_model: AppModel):
+        super(SolarViewerWidget, self).__init__()
+        self.setMinimumSize(600, 600)
+        self.setMaximumSize(600, 600)
+        self.setMouseTracking(True)
+        self.__scene = QGraphicsScene(self)
+        self.__scene.setSceneRect(0, 0, 600, 600)
+        self.__view = QGraphicsView(self.__scene, self)
+        self.__view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.__view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.__view.setAttribute(Qt.WA_TransparentForMouseEvents)
+        view_rect = self.contentsRect()
+        self.__view.setSceneRect(0, 0, view_rect.width(), view_rect.height())
+        self.__view.show()
+        
+        self.__label: QLabel = QLabel()
+        self.__pixmap = QPixmap(600, 600)
+        self.__offset: QPoint = QPoint(0, 0)
+        self.__is_moved: bool = False
+
+
+        self.__previous_x: int = 0
+        self.__previous_y: int = 0
+
+        self.__create_pixmap()
+
+
+    def __create_pixmap(self) -> QGraphicsProxyWidget:
+        default_plot = QPixmap(600, 600)
+        default_plot.fill(Qt.green)
+        self.__label.setPixmap(default_plot)
+        return self.__scene.addWidget(self.__label)
+
+    def update_widget(self):
+        self.update()
+
+    def paintEvent(self, a0: QtGui.QPaintEvent) -> None:
+        painter = QPainter()
+        painter.begin(self)
+        self.__draw_solar_frame(painter)
+        self.__draw_border_of_zone_interesting(painter)
+        painter.end()    
+
+    def set_solar_frame_to_draw(self, pixmap: QPixmap, offset: QPoint) -> None:
+        self.__pixmap = pixmap
+        self.__offset = offset
+
+
+    def __draw_solar_frame(self, painter: QPainter) -> None:
+        pass
+        #self.__label.setPixmap(self.__pixmap)
+        #self.__label.pos = self.__offset
+
+    def __draw_border_of_zone_interesting(self, painter: QPainter) -> None:
+        pass
+
+
+    def mousePressEvent(self, a0: QtGui.QMouseEvent) -> None:
+        self.__is_moved = True
+
+    def mouseReleaseEvent(self, a0: QtGui.QMouseEvent) -> None:
+        self.__is_moved = False
+
+    def mouseMoveEvent(self, event: QtGui.QMouseEvent) -> None:
+        current_x = event.x()
+        current_y = event.y()
+        if self.__is_moved:
+            delta_x = current_x - self.__previous_x
+            delta_y = current_y - self.__previous_y
+            self.mouseMoveSignal.emit(delta_x, delta_y)
+        self.__previous_x = current_x
+        self.__previous_y = current_y
+
+    def wheelEvent(self, event: QtGui.QWheelEvent) -> None:
+        pass
+    '''
     def __init__(self, solar_viewer_controller, app_model: AppModel):
         self.__zone_interesting_model: ZoneInteresting = app_model.zone_interesting
         self.__controller = solar_viewer_controller
@@ -52,9 +129,6 @@ class SolarViewerWidget(QWidget):
 
 
     def __draw_border_of_zone_interesting(self, painter: QPainter) -> None:
-        
-        #painter.setPen(QPen(Qt.red, 5.0, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
-
         borderPen = QPen(Qt.red, 2.0, Qt.DotLine)
         diagonalPen = QPen(Qt.blue, 3.0, Qt.DotLine)
         painter.setPen(borderPen)
@@ -74,9 +148,11 @@ class SolarViewerWidget(QWidget):
 
 
     def mousePressEvent(self, a0: QtGui.QMouseEvent) -> None:
+        print("press")
         self.__is_moved = True
 
     def mouseReleaseEvent(self, a0: QtGui.QMouseEvent) -> None:
+        print("release")
         self.__is_moved = False
 
     def mouseMoveEvent(self, event: QtGui.QMouseEvent) -> None:
@@ -91,3 +167,5 @@ class SolarViewerWidget(QWidget):
 
     def wheelEvent(self, event: QtGui.QWheelEvent) -> None:
         self.wheelScrollSignal.emit(event.angleDelta().x(), event.angleDelta().y())
+    
+    '''
