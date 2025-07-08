f1d035dbb2d53d9fba9f49099c0316b2d853a9ee
Teoretik <kobzarev.dani@yandex.ru>
2025-07-08 22:10:12 +0400

    PyInstaller build, step 3

--- hooks/hook-astropy.py
+++ hooks/hook-astropy.py
@@ -1,12 +1,2 @@
-# from PyInstaller.utils.hooks import collect_submodules
-
-
-# hiddenimports = [
-#     'astropy.tests.helper',
-#     'astropy.tests.runner',
-#     *collect_submodules('astropy.tests')
-
-# ]
-
 from PyInstaller.utils.hooks import collect_submodules
 hiddenimports = collect_submodules('astropy')
--- hooks/hook-sunpy.py
+++ hooks/hook-sunpy.py
@@ -1,8 +1,2 @@
-# from PyInstaller.utils.hooks import collect_data_files, collect_submodules
-
-# datas = collect_data_files('sunpy')
-# hiddenimports = collect_submodules('sunpy')
-
-
 from PyInstaller.utils.hooks import collect_data_files
 datas = collect_data_files('sunpy')
